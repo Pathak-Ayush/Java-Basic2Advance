455fd408931f04d2e5b3a0253c5618c9adf08435
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-06 12:19:26 +0530

    first commit

---
+++ classcode/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+	public int Sum(int a,int b){
+		return  a+b;
+	}
+
+	public static void main(String[] args) {
+		Calculator obj1 = new Calculator();
+		int sumData =obj1.Sum(23,234);
+		System.out.println("The sum of the two number is "+ sumData);
+	}
+}
+
+
+//access modifiers in the java
+// dp3
+// default
+// public
+//private
+// protechte
