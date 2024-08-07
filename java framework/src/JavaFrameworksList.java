6fb9e60b5088cfae0a4a1a46c7cc27ef0c1be2f8
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-07 10:04:55 +0530

    intro to class

---
+++ java framework/src/JavaFrameworksList.java
@@ -0,0 +1,28 @@
+public class JavaFrameworksList {
+	public static void main(String[] args) {
+
+	}
+}
+
+
+/*
+java framework list for  development of the  desktop application
+
+⭐⭐⭐⭐ JavaFX
+
+JavaFX is a powerful and versatile framework for building visually appealing desktop applications, with a rich set of
+		UI controls,
+		smooth animations,
+		and support for multimedia
+It is compatible with Java and popular IDEs like Eclipse and IntelliJ.
+
+
+
+// 2  Vaadin
+
+Vaadin is suitable for desktop application development, especially for web developers familiar with HTML, CSS, and JavaScript. It allows them to create desktop applications using their existing skill set.
+
+
+//awt -> Abstract window toolkit
+AWT is the oldest Java GUI toolkit and serves as the foundation for both Swing and JavaFX. It provides a basic set of UI components that work across platforms.
+ */
---
+++ oops/classes/src/IntroClass.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class IntroClass {
+
+	// Define the Animal class as a static inner class
+	public static class Animal {
+		// fields -> this is also know as the data member and attributes of the class
+		String name;
+		int age;
+		float weight;
+
+		// Create the constructor
+		//  is created while creating the object of the
+		public Animal(String name, int age, float weight) {
+			this.name = name;
+			this.age = age;
+			this.weight = weight;
+		}
+
+		// Method to display animal information
+		public void displayInfo() {
+			System.out.println("Name of the animal: " + name + "\nWeight: " + weight + "\nAge: " + age);
+		}
+	}
+
+	public static void main(String[] args) {
+		// Create a Scanner object to take user input
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the name of the animal:");
+		String name = sc.next();
+
+		System.out.println("Enter the age of the animal:");
+		int age = sc.nextInt();
+
+		System.out.println("Enter the weight of the animal:");
+		float weight = sc.nextFloat(); // Changed to nextFloat() to match the type of weight
+
+//		 object of the Animal
+		Animal obj = new Animal(name, age, weight);
+
+//
+		obj.displayInfo();
+	}
+}
