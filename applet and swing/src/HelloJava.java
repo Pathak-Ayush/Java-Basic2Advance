cd46753bd545276acfc8d8e5aa79c4d873245840
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-12 12:20:55 +0530

    check cuboid is cube or not

---
+++ applet and swing/src/HelloJava.java
@@ -0,0 +1,16 @@
+import java.applet.*;
+import java.awt.*;
+
+public class HelloJava extends Applet {
+	public void init() {
+		// Initialize the applet
+	}
+
+	public void start() {
+		// Start the applet
+	}
+
+	public void paint(Graphics g) {
+		g.drawString("Welcome Ashutosh!", 10, 100);
+	}
+}
--- classcode/src/Cuboid.java
+++ classcode/src/Cuboid.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
 
 public class Cuboid {
-//	data member of the class
+	// data member of the class
+	static double length;
+	static double breadth;
+	static double height;
 
-
- 	static double  length;
-	static  double breadth ;
-	 static  double height;
-
-	Cuboid(double length,double breadth,double height){
-		this.length= length;
+	Cuboid(double length, double breadth, double height) {
+		this.length = length;
 		this.breadth = breadth;
 		this.height = height;
 	}
 
-	public  static double volume(){
-		double volume = length* breadth * height;
+	public static double volume() {
+		double volume = length * breadth * height;
 		return volume;
 	}
+
+	public static boolean isCube() {
+		return length == breadth && breadth == height;
+	}
+
 	public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-		System.out.println("Enter the  of the cuboid");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of the cuboid");
 		double length = sc.nextDouble();
-		System.out.println("Enter the height of the cuboid");
-		double breadth =sc.nextDouble();
+		System.out.println("Enter the breadth of the cuboid");
+		double breadth = sc.nextDouble();
 		System.out.println("Enter the height of the cuboid");
 		double height = sc.nextDouble();
 
-//		create the object of the class Cuboid
-		Cuboid  obj1 = new Cuboid(length,breadth,height);
-		System.out.println("volume of the cuboid :"+obj1.volume());
+		// create the object of the class Cuboid
+		Cuboid obj1 = new Cuboid(length, breadth, height);
+		System.out.println("Volume of the cuboid: " + obj1.volume());
+		System.out.println("Is it a cube? " + obj1.isCube());
 	}
 }
