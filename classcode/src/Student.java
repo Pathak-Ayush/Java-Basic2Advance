9a8a31924939481531ffece8248d01fba84a2427
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-08-14 11:35:15 +0530

    modify Student problem

--- classcode/src/Student.java
+++ classcode/src/Student.java
@@ -4,6 +4,16 @@ public class Student {
 	private int mark2;
 	private int mark3;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
 	public int getMark2() {
 		return mark2;
 	}
@@ -40,11 +50,19 @@ public class Student {
 		int totalMarks = student.getMarks1() + student.getMark2() + student.getMark3();
 		System.out.println("Total marks: " + totalMarks);
 	}
+	public int getTotalMarks() {
+		return marks1 + mark2 + mark3;
+	}
 
+	public String toString(){
+		return String.format("Student Id: %d\nStudent Name: %s\nMarks in Subject 1: %d\nMarks in Subject 2: %d\nMarks in Subject 3: %d\n Total Marks : %d" ,
+				this.id, this.name, this.marks1, this.mark2, this.mark3,this.getTotalMarks());
+	}
 	public static void printStudentInfo(Student student) {
 		System.out.println("Student Information:");
 		System.out.println("--------------------");
 		System.out.println("Student Id: " + student.getId());
+		System.out.println("Student Name: "+student.getName());
 		System.out.println("Marks in Subject 1: " + student.getMarks1());
 		System.out.println("Marks in Subject 2: " + student.getMark2());
 		System.out.println("Marks in Subject 3: " + student.getMark3());
@@ -56,9 +74,12 @@ public class Student {
 	public static void main(String[] args) {
 		Student student = new Student();
 		student.setId(1);
+		student.setName("Ashutosh kumar");
 		student.setMarks1(80);
 		student.setMark2(70);
 		student.setMark3(90);
-		printStudentInfo(student);
+//		printStudentInfo(student);
+		System.out.println(student);
+
 	}
 }
